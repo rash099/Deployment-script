a503569c1661601c637744f251a86589052112b0
rash099 <zrashi014@gmail.com>
2024-07-19 16:38:12 +0530

    Update jenkins_linus.sh

--- jenkins_linus.sh
+++ jenkins_linus.sh
@@ -4,7 +4,7 @@ sudo yum update -y
 sudo yum install openjdk-11-jdk -y
 sudo yum update -y
 sudo yum install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
